de1e15fe608145db16476afbee500d4735adaa00
thanhtrinh2003 <thanhtrinh280506@gmail.com>
2025-12-20 12:37:25 -0600

    update bio

--- src/components/About/AboutSection.jsx
+++ src/components/About/AboutSection.jsx
@@ -12,7 +12,7 @@ const AboutSection = () => {
     <>
       <SectionTitle>ABOUT ME</SectionTitle>
       <SummaryText>
-        I'm a CS student at Georgia Tech with specialization in System
+        I'm an incoming SWE @ coinbase, specifically performing smart contracts work on base. I graduated as from Georgia Tech's CS program with specialization in System
         Architecture and Theory.
       </SummaryText>
       <SummaryText>Currently, I'm super into blockchain and defi for at least the next 5 years.</SummaryText>
